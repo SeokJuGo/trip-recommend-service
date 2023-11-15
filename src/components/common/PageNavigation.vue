e57efa579b15dd43af240e989c3081a680f7ad40
조시훈 <kepler452k@naver.com>
2023-11-16 00:58:04 +0900

    fix(PageNavigation.vue): Fix PageNavigation.vue as parameters are changed

--- src/components/common/PageNavigation.vue
+++ src/components/common/PageNavigation.vue
@@ -2,48 +2,50 @@
 import { computed } from "vue";
 
 defineProps({
-    pageNum: Number,
-    totalPages: Number,
+  pageNum: Number,
+  totalPages: Number,
 });
 </script>
 
 <template>
-    <div>
-        <ul class="pagination justify-content-center">
-            <li class="page-item">
-                <a class="page-link" @click="$emit('onChangePageNum', 1)">최신</a>
-            </li>
-            <!-- <li class="page-item">
-                <a class="page-link" @click="onPageChange(1)">최신</a>
-            </li>
-            <li class="page-item">
-                <a class="page-link" @click="onPageChange(startPage == 1 ? 1 : startPage - 1)"
-                    >이전</a
-                >
-            </li>
-            <template v-for="pg in range(startPage, endPage)" :key="pg">
-                <li :class="currentPage === pg ? 'page-item active' : 'page-item'">
-                    <a class="page-link" @click="onPageChange(pg)">{{ pg }}</a>
-                </li>
-            </template>
-            <li class="page-item">
-                <a class="page-link" @click="onPageChange(endRange ? totalPage : endPage + 1)"
-                    >다음</a
-                >
-            </li>
-            <li class="page-item">
-                <a class="page-link" @click="onPageChange(totalPage)">마지막</a>
-            </li> -->
-            <li class="page-item">
-                <a class="page-link" @click="$emit('onChangePageNum', totalPages)">마지막</a>
-            </li>
-        </ul>
-    </div>
+  <div>
+    <ul class="pagination justify-content-center">
+      <li class="page-item">
+        <a class="page-link" @click="$emit('onChangePageNum', 1)">최신</a>
+      </li>
+      <li class="page-item">
+        <a class="page-link" @click="$emit('onChangePageNum', pageNum == 1 ? 1 : pageNum - 1)">이전</a>
+      </li>
+      <!--
+        <li class="page-item">
+          <a class="page-link" @click="onPageChange(startPage == 1 ? 1 : startPage - 1)"
+          >이전</a
+          >
+        </li>
+        <template v-for="pg in range(startPage, endPage)" :key="pg">
+          <li :class="currentPage === pg ? 'page-item active' : 'page-item'">
+            <a class="page-link" @click="onPageChange(pg)">{{ pg }}</a>
+          </li>
+        </template>
+        <li class="page-item">
+          <a class="page-link" @click="onPageChange(endRange ? totalPage : endPage + 1)"
+          >다음</a
+          >
+        </li>
+      -->
+      <li class="page-item">
+        <a class="page-link" @click="$emit('onChangePageNum', pageNum == totalPages ? totalPages : pageNum + 1)">다음</a>
+      </li>
+      <li class="page-item">
+        <a class="page-link" @click="$emit('onChangePageNum', totalPages)">마지막</a>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <style scoped>
 a {
-    cursor: pointer;
+  cursor: pointer;
 }
 </style>
 <!-- <script setup>
