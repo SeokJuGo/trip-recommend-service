c047c8d2a69ad6ad09ec5b73374f411c195ce95d
SeokJu <116260619+SeokJuGo@users.noreply.github.com>
2023-11-17 17:54:55 +0900

    fix(user.js): parameter user->username

--- src/api/user.js
+++ src/api/user.js
@@ -2,8 +2,8 @@ import { localAxios } from "@/util/http-commons";
 
 const axios = localAxios();
 
-async function login(user, success, fail) {
-  await axios.post(`/auth/login`, JSON.stringify(user)).then(success).catch(fail);
+async function login(username, success, fail) {
+  await axios.post(`/auth/login`, JSON.stringify(username)).then(success).catch(fail);
 }
 
 async function findById(username, success, fail) {
@@ -11,18 +11,18 @@ async function findById(username, success, fail) {
   await axios.get(`/user/${username}`).then(success).catch(fail);
 }
 
-async function tokenRegeneration(user, success, fail) {
+async function tokenRegeneration(username, success, fail) {
   axios.defaults.headers["refresh-token"] =
     sessionStorage.getItem("refresh-token"); //axios header에 refresh-token 셋팅
-  await axios.post(`/auth/refresh`, user).then(success).catch(fail);
+  await axios.post(`/auth/refresh`, username).then(success).catch(fail);
 }
 
 async function logout(username, success, fail) {
   await axios.get(`/auth/logout/${username}`).then(success).catch(fail);
 }
 
-async function modify(user, success, fail) {
-  await axios.put(`/user/`, user).then(success).catch(fail);
+async function modify(username, success, fail) {
+  await axios.put(`/user/`, username).then(success).catch(fail);
 }
 
 async function deleteUser(username, success, fail) {
