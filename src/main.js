54ec6bd06d30de2edc28e7f26ef26385619c13eb
SeokJu <116260619+SeokJuGo@users.noreply.github.com>
2023-11-20 17:55:45 +0900

    fix(KaKaoMap.vue): 마우스를 올려도 title이 안나오는 현상 해결

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
 //import './assets/main.css'
-import "bootstrap/dist/css/bootstrap.min.css"
-import "bootstrap"
+
+import BootstrapVue3 from 'bootstrap-vue-3'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 import {library} from '@fortawesome/fontawesome-svg-core'
@@ -8,9 +10,8 @@ import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
 import {fas} from '@fortawesome/free-solid-svg-icons'
 import {far} from '@fortawesome/free-regular-svg-icons'
 import {fab} from '@fortawesome/free-brands-svg-icons'
-
-
 import Antd from 'ant-design-vue';
+
 
 import 'ant-design-vue/dist/reset.css';
 
@@ -24,4 +25,7 @@ app.use(createPinia())
 app.use(router)
 app.component('font-awesome-icon',FontAwesomeIcon)
 app.use(Antd)
+
+app.use(BootstrapVue3)
 app.mount('#app')
+
