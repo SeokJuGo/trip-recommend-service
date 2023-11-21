d402003f31451e6ad3f0b8e9d99617c86e931836
SeokJu <116260619+SeokJuGo@users.noreply.github.com>
2023-11-21 11:42:37 +0900

    fix(BootStrap): vue3 일부기능 Bootstrap 오작동 수정

--- src/main.js
+++ src/main.js
@@ -1,29 +1,31 @@
 //import './assets/main.css'
+import BootstrapVue from 'bootstrap-vue-3'
+import "bootstrap/dist/css/bootstrap.min.css";
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+import "bootstrap";
+import { createApp } from "vue";
+import { createPinia } from "pinia";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+import { far } from "@fortawesome/free-regular-svg-icons";
+import { fab } from "@fortawesome/free-brands-svg-icons";
 
-import BootstrapVue3 from 'bootstrap-vue-3'
-import 'bootstrap/dist/css/bootstrap.css'
-import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import {library} from '@fortawesome/fontawesome-svg-core'
-import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
-import {fas} from '@fortawesome/free-solid-svg-icons'
-import {far} from '@fortawesome/free-regular-svg-icons'
-import {fab} from '@fortawesome/free-brands-svg-icons'
-import Antd from 'ant-design-vue';
+import Antd from "ant-design-vue";
 
+import "ant-design-vue/dist/reset.css";
 
-import 'ant-design-vue/dist/reset.css';
+import App from "./App.vue";
+import router from "./router";
 
 library.add(fas, far, fab);
 
 const app = createApp(App);
 
-app.use(createPinia())
-app.use(router)
-app.component('font-awesome-icon',FontAwesomeIcon)
-app.use(Antd)
+app.use(createPinia());
+app.use(router);
+app.use(BootstrapVue);
+app.component("font-awesome-icon", FontAwesomeIcon);
+app.use(Antd);
 
-app.use(BootstrapVue3)
-app.mount('#app')
-
+app.mount("#app");
